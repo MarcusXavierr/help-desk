debbe3e73c6abfad1c27d0315b1328aa186287aa
Marcus Xavier <marcusxavierr123@gmail.com>
2020-06-23 11:32:16 -0300

    create calls migration

---
+++ database/migrations/2020_06_23_140404_create_calls_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCallsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calls', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            //call
+            $table->string('title');
+            $table->string('category');
+            $table->text('description');
+
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calls');
+    }
+}
