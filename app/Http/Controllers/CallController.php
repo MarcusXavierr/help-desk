15adcac5ad517f6b33b2344154a9b9fa3f1a0c39
Marcus Xavier <marcusxavierr123@gmail.com>
2020-06-24 13:15:40 -0300

    Create store function

--- app/Http/Controllers/CallController.php
+++ app/Http/Controllers/CallController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Call;
 
 class CallController extends Controller
 {
-    //
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    public function index()
+    {
+        return view('call.index');
+    }
+
+    public function create()
+    {
+        return view('call.create');
+    }
+
+    public function store(Request $request)
+    {   
+        $input = $request->all();
+        return Call::create($input);
+        
+    }
 }
