d201fb36911bf717b5ffaa0093b3f488cb6ed6eb
Marcus Xavier <marcusxavierr123@gmail.com>
2020-06-24 14:41:03 -0300

    Modify home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+<div class="container">    
+    <div class="row">
+      <div class="card-home">
+        <div class="card">
+          <div class="card-header">
+            Menu
+          </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-6 d-flex justify-content-center">
+                    <a href="{{route('call.create')}}">
+                        <img src="/images/formulario_abrir_chamado.png" width="70" height="70">
+                    </a>
+              </div>
+              <div class="col-6 d-flex justify-content-center">
+                <a href="{{route('call.index')}}">
+                    <img src="/images/formulario_consultar_chamado.png" width="70" height="70">
+                </a>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-</div>
+      </div>
+  </div>
 @endsection
