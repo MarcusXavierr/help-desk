16fac4063562698866744bc8239ba1572fc82a0f
Marcus Xavier <marcusxavierr123@gmail.com>
2020-06-24 14:40:35 -0300

    Add destroy method

--- app/Http/Controllers/CallController.php
+++ app/Http/Controllers/CallController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Call;
 
 class CallController extends Controller
@@ -14,7 +15,9 @@ class CallController extends Controller
 
     public function index()
     {
-        return view('call.index');
+        $calls = Call::all(['user_name','user_id','title','category','description','created_at','id'])->sortBy('created_at');
+        
+        return view('call.index',compact('calls'));
     }
 
     public function create()
@@ -25,7 +28,18 @@ class CallController extends Controller
     public function store(Request $request)
     {   
         $input = $request->all();
-        return Call::create($input);
+        $user_id = $input['user_id'];//Get the user id
+        $username = DB::table('users')->where('id',"$user_id")->get('name');//Get the user name
+        $input['user_name'] = $username[0]->name;//Save the user name
+        Call::create($input);
+        return redirect('/call');
         
+    }
+
+    public function destroy($id)
+    {
+        $call = Call::find($id);
+        $call->delete();
+        return redirect('/');
     }
 }
